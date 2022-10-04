5fa6e06003401e29587dbe683bd3d98adfeff025
Emmanuel John Ayarma <emmanueladane52@gmail.com>
2022-10-04 23:03:45 +0000

    add few features to the login design

--- starFrontend/src/user.js
+++ starFrontend/src/user.js
@@ -24,41 +24,50 @@ passwordCheck.addEventListener("click",()=>{
 //login function 
 const loginButton=document.querySelector('.loginButton')
 let LoginEmail=document.querySelector(".loginEmail")
-let LoginPassword=document.querySelector(".loginPassword")
+let LoginPassword=document.querySelector(".L_password")
 //let User=document.querySelector('.name')
 loginButton.addEventListener("click",()=>{
     email=LoginEmail.value
     password=LoginPassword.value
     alert(email,password)
-    //const data={email,password}
-    //axios.post("http://localhost:5000/authentication/login",data)
-    //.then((res)=>{
-    //      console.log(res)
-    // })
-    // .catch((err)=>{
-    //      console.log("an error occured")
-    // })
+    const data={email,password}
+    axios.post("http://localhost:5000/authentication/login",data)
+    .then((res)=>{
+          console.log(res)
+     })
+     .catch((err)=>{
+          const loginwarn=document.querySelector(".loginwarn")
+          if(err){
+            loginwarn.style.display="flex"
+          }
+     })
 })
 
 
 
 //Sign Up function
-const Sign_button=document.querySelector(".S_signUp")
-let Sign_firstname=document.querySelector(".S_firstname")
-let Sign_lastname=document.querySelector(".S_lastname")
-let Sign_email=document.querySelector(".S_email")
-let Sign_password=document.querySelector(".S_password")
-Sign_button.addEventListener("click",()=>{
-   firstname=Sign_firstname.value
-   lastname=Sign_lastname.value
-   email=Sign_email.value
-   password=Sign_password.value
+const signButton=document.querySelector(".signButton")
+let signFirstname=document.querySelector(".signFirstname")
+let signLastname=document.querySelector(".signLastname")
+let signEmail=document.querySelector(".signEmail")
+let signPassword=document.querySelector(".signPassword")
+
+signButton.addEventListener("click",()=>{
+   firstname=signFirstname.value
+   lastname=signLastname.value
+   email=signEmail.value
+   password=signPassword.value
    const data={firstname,lastname,email,password}
+   alert(data)
    axios.post("http://localhost:5000/authentication/register",data)
    .then((res)=>{
          console.log(res)
    })
    .catch((err)=>{
-      console.log("an error occured");
+      const signwarn=document.querySelector(".signwarn")
+      if(err){
+         signwarn.style.display="flex"
+      }
    })
 })
+
